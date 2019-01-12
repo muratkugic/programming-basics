63a91a0dfb8995a2f5b72640fb7b71203d8a1ee5
Murat Kugic <m.kugic@gmail.com>
2019-01-12 15:51:02 +0100

    added basic if-else logic

--- Conditionals/ConsoleUI/Program.cs
+++ Conditionals/ConsoleUI/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            if (firstName.ToLower() == "murat" && lastName.ToLower() == "kugic")
+            {
+                Console.WriteLine($"Greetings { firstName }!");
+            }
+            else if (firstName.ToLower() == "stefan" && lastName.ToLower() == "isgren" || lastName.ToLower() == "isen")
+            {
+                Console.WriteLine($"Howdy Mr. { lastName }!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello stranger: [{ firstName } { lastName }]");
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
