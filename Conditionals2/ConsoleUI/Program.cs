2e7f8f0a2606617ced584a6544c3daca6348e77d
Murat Kugic <m.kugic@gmail.com>
2019-01-12 16:06:29 +0100

    added second if-else exercise

---
+++ Conditionals2/ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age <= 16)
+            {
+                Console.WriteLine("Junior: 4.99$");
+            }
+            else if (age > 16 && age <= 64 )
+            {
+                Console.WriteLine("Adult: 14.99$");
+            }
+            else
+            {
+                Console.WriteLine("Senior: 9.99$");
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
