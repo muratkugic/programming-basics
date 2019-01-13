aa7ac7acc161d67c10d4991c44c374436232526e
Murat Kugic <m.kugic@gmail.com>
2019-01-13 08:15:01 +0100

    experimented with TypeConversioin and TryParse

---
+++ TypeConverison/ConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How old are you: ");
+            string ageText = Console.ReadLine();
+
+            bool isValidAge = int.TryParse(ageText, out int age);
+
+            if (isValidAge)
+            {
+                if (age >= 21)
+                {
+                    Console.WriteLine("You can enter.");
+                }
+                else
+                {
+                    Console.WriteLine($"Try in { 21 - age } year/s again.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Format '{ ageText }' unknown, please use whole numbers.");
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
