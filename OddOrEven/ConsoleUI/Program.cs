36b3a8b194afd25e1f9cc1f3cfd0930a36039a5b
Murat Kugic <m.kugic@gmail.com>
2019-01-14 22:25:41 +0100

    Added odd or even exercise

---
+++ OddOrEven/ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if a number is odd or even\n");
+
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"Number { num } is even.");
+            }
+            else
+            {
+                Console.WriteLine($"Number { num } is odd.");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
