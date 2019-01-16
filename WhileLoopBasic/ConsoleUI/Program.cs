a9ef025b7e42ac44d609b3d512c1bb83483e24d9
Murat Kugic <m.kugic@gmail.com>
2019-01-16 19:01:17 +0100

    added WhileLoop basic exercise

---
+++ WhileLoopBasic/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+
+            while (userInput.ToLower() != "q")
+            {
+                Console.Write("Ener your name (press 'q' to quit): ");
+                userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "q")
+                {
+                    Console.WriteLine("You pressed 'q' and the program terminated!");
+                    break;
+                }
+                else if (userInput.ToLower() == "murat")
+                {
+                    Console.WriteLine($"{ userInput } created this program!");
+                }
+                else
+                {
+                    Console.WriteLine($"Greetings { userInput }! Hope you enjoyed your stay!");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
