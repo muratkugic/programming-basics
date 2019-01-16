6f67e4c886e7a9cc15a276e39fcd4b5dcf10c437
Murat Kugic <m.kugic@gmail.com>
2019-01-16 19:00:41 +0100

    added DoLoop basic exercise

---
+++ DoLoopBasic/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+
+            do
+            {
+                Console.Write("Enter your name (type 'exit' to quit): ");
+                userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "exit")
+                {
+                    Console.WriteLine("You typed 'exit' and the program terminated.");
+                    break;
+                }
+                else if (userInput.ToLower() == "murat")
+                {
+                    Console.WriteLine($"The creator of this program is { userInput }!");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello { userInput }, glad you stoped by.");
+                }
+
+            } while (userInput.ToLower() != "exit");
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
