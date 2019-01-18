7c8c6ee99d267637b01b807dcf964f9402b504e5
Murat Kugic <m.kugic@gmail.com>
2019-01-18 22:52:09 +0100

    Added basic list exercise

---
+++ ListBasics/ConsoleUI/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>
+            {
+                "Bob",
+                "Tim",
+                "Sue",
+                "Jim"
+            };
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorting list:");
+            names.Sort();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("IndexOf:");
+            var index = names.IndexOf("Sue");
+            
+            Console.WriteLine($"Name: { names[index] } Index: { index }");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Adding/Removing to/from list:");
+
+            names.Add("Rex");
+            names.Remove("Jim");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
