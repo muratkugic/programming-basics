f98d1fabe4ea80a144bbf152a855d1875fbaa1b5
Murat Kugic <m.kugic@gmail.com>
2019-01-20 10:15:29 +0100

    added sictionary basic exercise

---
+++ DictionaryBasics/ConsoleUI/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create emplyee dictionary with employee ID as key and employee name as value.
+            // Ask for user inut "ID" and display employee name to Console.
+
+            Dictionary<int, string> employees = new Dictionary<int, string>
+            {
+                [1] = "Sue",
+                [2] = "Bob",
+                [3] = "Jim",
+                [4] = "Tim"
+            };
+
+            Console.Write("Enter employee ID: ");
+            var userInput = Console.ReadLine();
+
+            bool isValidId = int.TryParse(userInput, out int id);
+
+            if (isValidId)
+            {
+                if (id == 1)
+                {
+                    Console.WriteLine($"You enterd '{ userInput }' and the name is: { employees[1] }."); 
+                }
+                else if (id == 2)
+                {
+                    Console.WriteLine($"You enterd '{ userInput }' and the name is: { employees[2] }.");
+                }
+                else if (id == 3)
+                {
+                    Console.WriteLine($"You enterd '{ userInput }' and the name is: { employees[3] }.");
+                }
+                else if (id == 4)
+                {
+                    Console.WriteLine($"You enterd '{ userInput }' and the name is: { employees[4] }.");
+                }
+                else
+                {
+                    Console.WriteLine($"'{ id }' is not present in the dictionary.");
+                    Console.WriteLine($"The dictionary lenght is: { employees.Count }");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"'{ userInput }' is not a valid employee ID!");
+            }
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
