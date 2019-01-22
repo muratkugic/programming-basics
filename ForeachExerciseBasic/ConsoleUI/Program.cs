ebed61352528cffdd38521d933a01a630c3d3768
Murat Kugic <m.kugic@gmail.com>
2019-01-22 19:48:16 +0100

    added basic foreach exercise

---
+++ ForeachExerciseBasic/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for their first name. Continue asking until there are no more users.
+            // Add names to a list of string and loop through the list greeting every
+            // user "Hello <name>!".
+
+            var firstName = new List<string>();
+            var continueAsking = "";
+
+            do
+            {
+                Console.Write("Enter your first name: ");
+                var userInput = Console.ReadLine();
+
+                Console.Write("Do you want to add anther name? Type 'yes' or 'no' to quit: ");
+                continueAsking = Console.ReadLine();
+
+                firstName.Add(userInput);
+            } while (continueAsking.ToLower() == "yes");
+
+            foreach (var name in firstName)
+            {
+                Console.WriteLine($"Hello { name }!");
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
