b11236a70ff2916a7a11a59a51ca05b417af2eba
Murat Kugic <m.kugic@gmail.com>
2019-01-27 18:37:41 +0100

    added basic number switch exercise

---
+++ SwitchNumbers/ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchNumbers(24, 12);
+
+            Console.ReadLine();
+        }
+
+        private static void SwitchNumbers(int num1, int num2)
+        {
+            Console.WriteLine("Before the switch!");
+            Console.WriteLine($"Num1: { num1 } Num2: { num2 }");
+
+            var temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("After the switch!");
+            Console.WriteLine($"Num1: { num1 } Num2: { num2 }");
+        }
+    }
+}
